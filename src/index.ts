b349c3c15faa5953108887c4850bf2b3e3c43eca
Geison Oriani <eu@geison.dev>
2025-08-07 20:03:31 -0300

    feat: add error handling middleware and catch-all route for 404 errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import 'dotenv/config'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import routes from './routes'
 
 const app = express()
@@ -24,6 +24,27 @@ app.get('/status', (_req, res) => {
 
 // Routes
 routes(app)
+
+// Catch-all route for 404 errors
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not Found',
+    message: `The requested URL ${req.originalUrl} was not found on this server.`
+  })
+})
+
+// Global error handler
+interface ErrorWithStack extends Error {
+  stack?: string
+}
+
+app.use((err: ErrorWithStack, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack)
+  res.status(500).json({
+    error: 'Internal Server Error',
+    message: 'An unexpected error occurred.'
+  })
+})
 
 const server = app.listen(3000, () =>
   console.log(`Server is running on http://127.0.0.1:${3000}`),
